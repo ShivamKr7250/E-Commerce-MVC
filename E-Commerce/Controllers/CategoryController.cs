315b90e4c14f1eb96475677f71205461a1d3bc8a
$HIVAM <supshiv7250@gmail.com>
2023-10-04 23:56:29 +0530

    Added Category Index

---
+++ E-Commerce/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using E_Commerce.Data;
+using E_Commerce.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_Commerce.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCAtegory = _db.Categories.ToList();
+            return View();
+        }
+    }
+}
