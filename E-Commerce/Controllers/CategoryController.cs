c26ef42f7014d047457c5eb7ff68b2b3d3d1336e
$HIVAM <supshiv7250@gmail.com>
2023-10-06 09:01:11 +0530

    Category CRUD Operations Done

--- E-Commerce/Controllers/CategoryController.cs
+++ E-Commerce/Controllers/CategoryController.cs
@@ -38,6 +38,7 @@ namespace E_Commerce.Controllers
             {
                 _db.Categories.Add(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category Created Successfully";
                 return RedirectToAction("Index", "Category");
             }
             return View();
@@ -52,32 +53,59 @@ namespace E_Commerce.Controllers
             }
             //Multiple Ways to Retrieve Data and Edit 
 
-            Category categoryFromDb = _db.Categories.Find(id);
-            if ((categoryFromDb == null){
+            Category? categoryFromDb = _db.Categories.Find(id); //Find work only on primary key
+           // Category? categoryFromDb1 = _db.Categories.FirstOrDefault(u=>u.Id==id);
+            // Category? categoryFromDb2 = _db.Categories.Where(u=>u.Id==id).FirstOrDefault();
+            if (categoryFromDb == null){
                 return NotFound();
             }
-            return View();
+            return View(categoryFromDb);
         }
 
         [HttpPost]
         public IActionResult Edit(Category obj)
         {
-            if (obj.Name == obj.DisplayOrder.ToString())
-            {
-                ModelState.AddModelError("name", "The Display Order cannot exactly match the Name.");
-            }
-            //  if (obj.Name.ToLower() == "test")
-            //{
-            //  ModelState.AddModelError("", "Test is an Invalid Value");
-            //}
             if (ModelState.IsValid)
             {
-                _db.Categories.Add(obj);
+                _db.Categories.Update(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category Updated Successfully";
                 return RedirectToAction("Index", "Category");
             }
             return View();
 
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            //Multiple Ways to Retrieve Data and Edit 
+
+            Category? categoryFromDb = _db.Categories.Find(id); //Find work only on primary key
+                                                                // Category? categoryFromDb1 = _db.Categories.FirstOrDefault(u=>u.Id==id);
+                                                                // Category? categoryFromDb2 = _db.Categories.Where(u=>u.Id==id).FirstOrDefault();
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            Category obj = _db.Categories.Find(id); 
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            TempData["success"] = "Category Deleted Successfully";
+            return RedirectToAction("Index", "Category");
         }
     }
 }
