c45a9655dc39d279c5652887dca8e5194843ca29
$HIVAM <supshiv7250@gmail.com>
2023-10-29 16:35:36 +0530

    added OrderHeader and OrderDetails to DB

--- E-Commerce.DataAccess/Data/ApplicationDbContext.cs
+++ E-Commerce.DataAccess/Data/ApplicationDbContext.cs
@@ -13,14 +13,12 @@ namespace E_Commerce.DataAccess.Data
         }
 
         public DbSet<Category> Categories { get; set; }
-
         public DbSet<Product> Products { get; set; }    
-
         public DbSet<ApplicationUser> ApplicationUsers { get; set; }
-
         public DbSet<ShoppingCart> ShoppingCarts { get; set; }
-
         public DbSet<Company> Companies { get; set; }
+        public DbSet<OrderHeader> OrderHeaders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
