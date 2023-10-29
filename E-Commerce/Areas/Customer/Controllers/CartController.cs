ecc246597843a06850a99fbd885f3ab4f772f76b
$HIVAM <supshiv7250@gmail.com>
2023-10-29 22:38:29 +0530

    Payment Action Completed

--- E-Commerce/Areas/Customer/Controllers/CartController.cs
+++ E-Commerce/Areas/Customer/Controllers/CartController.cs
@@ -171,7 +171,7 @@ namespace E_Commerce.Areas.Customer.Controllers
 
 				if(session.PaymentStatus.ToLower() == "paid")
 				{
-					_unitOfWork.OrderHeader.UpdateStripePaymentID(ShoppingCartVM.OrderHeader.Id, session.Id, session.PaymentIntentId);
+					_unitOfWork.OrderHeader.UpdateStripePaymentID(id, session.Id, session.PaymentIntentId);
 					_unitOfWork.OrderHeader.UpdateStatus(id, SD.StatusApproved, SD.PaymentStatusApproved);
 					_unitOfWork.Save();
 				}
