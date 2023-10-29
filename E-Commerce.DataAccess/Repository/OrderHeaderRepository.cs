2fc7ff7e1d89782df651ae14a643118e30279a28
$HIVAM <supshiv7250@gmail.com>
2023-10-29 19:17:52 +0530

    Added Payment Id Details

--- E-Commerce.DataAccess/Repository/OrderHeaderRepository.cs
+++ E-Commerce.DataAccess/Repository/OrderHeaderRepository.cs
@@ -21,5 +21,32 @@ namespace E_Commerce.DataAccess.Repository
         {
            _db.OrderHeaders.Update(obj);
         }
-    }
+
+		public void UpdateStatus(int id, string orderStatus, string? paymentStatus = null)
+		{
+			var orderFromDb = _db.OrderHeaders.FirstOrDefault(x => x.Id == id);
+            if(orderFromDb == null)
+            {
+                orderFromDb.OrderStatus = orderStatus;
+                if (!string.IsNullOrEmpty(paymentStatus))
+                {
+                    orderFromDb.PaymentStatus = paymentStatus;
+                }
+            }
+		}
+
+		public void UpdateStripePaymentID(int id, string sessionId, string paymentIntentId)
+		{
+			var orderFromDb = _db.OrderHeaders.FirstOrDefault(x => x.Id == id);
+            if(!string.IsNullOrEmpty(sessionId))
+            {
+                orderFromDb.SessionId = sessionId;
+            }
+			if (!string.IsNullOrEmpty(paymentIntentId))
+			{
+				orderFromDb.PaymentIntentId = paymentIntentId;
+                orderFromDb.PaymentDate = DateTime.Now;
+			}
+		}
+	}
 }
--- E-Commerce.Models/OrderHeader.cs
+++ E-Commerce.Models/OrderHeader.cs
@@ -29,6 +29,7 @@ namespace E_Commerce.Models
         public DateTime PaymentDate { get; set; }
         public DateOnly PaymentDueDate { get; set; }
 
+        public string? SessionId { get; set; }
         public string? PaymentIntentId { get; set; }
 
         [Required]
