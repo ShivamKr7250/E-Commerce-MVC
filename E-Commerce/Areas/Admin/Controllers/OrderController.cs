5dd7d71082d4fad1960b401da8781b5fde8c62c3
$HIVAM <supshiv7250@gmail.com>
2023-10-30 09:46:26 +0530

    Added order UI

---
+++ E-Commerce/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,33 @@
+﻿using E_Commerce.DataAccess.Repository.IRepository;
+using E_Commerce.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_Commerce.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class OrderController : Controller
+	{
+		private readonly IUnitOfWork _unitOfWork;
+
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+		{
+			return View();
+		}
+
+		#region API CALLS
+		[HttpGet]
+		public IActionResult GetAll()
+		{
+			List<OrderHeader> objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+			return Json(new { data = objOrderHeaders });
+		}
+
+		#endregion
+	}
+}
