fcf143349dd9a4cdd252ec6c8d8e4d90f5257029
$HIVAM <supshiv7250@gmail.com>
2023-11-25 11:29:30 +0530

    List User 01

---
+++ E-Commerce/Areas/Admin/Controllers/UserController .cs
@@ -0,0 +1,47 @@
+﻿using E_Commerce.DataAccess.Data;
+using E_Commerce.DataAccess.Repository.IRepository;
+using E_Commerce.Models;
+using E_Commerce.Models.ViewModels;
+using E_Commerce.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_Commerce.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = SD.Role_Admin)]
+    public class UserController : Controller
+    {
+
+        private readonly ApplicationDbContext _db;
+
+        public UserController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        #region API CALLS
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<ApplicationUser> objUserList = _db.ApplicationUsers.Include(u => u.Company).ToList();
+            return Json(new {data = objUserList });
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int? id)
+        {
+
+            return Json(new { success = true, message = "Deleted Successful" });
+        }
+        #endregion
+    }
+}
---
+++ E-Commerce/wwwroot/js/user.js
@@ -0,0 +1,29 @@
+var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": {url :'/admin/user/getall' },
+        "columns": [
+            { data: 'name', "width" :"15%" },
+            { data: 'email', "width": "15%" },
+            { data: 'phoneNumber', "width": "15%" },
+            { data: 'company.name', "width": "15%" },
+            { data: '', "width": "15%" },
+            {
+                data: 'id',
+                "render": function (data) {
+                    return `<div class="w-75 btn-group" role="group">
+                    <a href="/admin/company/upsert?id=${data}" class="btn btn-primary mx-2"><i class="bi bi-pencil-square"></i> Edit</a>
+                    <a onClick=Delete('/admin/company/delete/${data}') class="btn btn-danger mx-2"><i class="bi bi-trash-fill"></i> Delete</a>
+                    </div>`
+                },
+                "width": "25%"
+            }
+        ]
+    });
+}
+
