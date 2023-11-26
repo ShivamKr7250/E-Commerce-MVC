91eb1e8b539f366dabe8f4872ea1cc3f9a89d560
$HIVAM <supshiv7250@gmail.com>
2023-11-26 11:09:03 +0530

    Display Image On Shopping Cart

--- E-Commerce/Areas/Customer/Controllers/CartController.cs
+++ E-Commerce/Areas/Customer/Controllers/CartController.cs
@@ -32,8 +32,11 @@ namespace E_Commerce.Areas.Customer.Controllers
 				OrderHeader = new()
 			};
 
+			IEnumerable<ProductImage> productImages = _unitOfWork.ProductImage.GetAll();
+
 			foreach (var cart in ShoppingCartVM.ShoppingCartList)
 			{
+				cart.Product.ProductImages = productImages.Where(u => u.ProductId == cart.Product.Id).ToList();
 				cart.Price = GetPriceBasedOnQuantity(cart);
 				ShoppingCartVM.OrderHeader.OrderTotal += (cart.Price * cart.Count);
 			}
